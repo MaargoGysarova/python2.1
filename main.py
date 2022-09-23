c3629ca336fc1f0730dfa59d51b98ae66bb34999
margogusarova <margogusarova19@gmail.com>
2022-09-23 13:02:20 +0400

    add function to get image from link

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+url1 = 'https://yandex.ru/images/search?text=tiger'
+url2 = 'https://yandex.ru/images/search?text=leopard'
+#response = requests.get(url1)
+#soup = BeautifulSoup(response.text, 'lxml')
+
+os.mkdir("dataset")
+os.mkdir("dataset/" + "tiger")
+os.mkdir("dataset/" + "leopard")
+
+#images = soup.find_all('img')
+
+def get_image_url(name):
+    i = 1
+    page = 0
+    response = requests.get("https://yandex.ru/images/search?p={page}&text={name}&lr=51&rpt=image")
+    data = []
+    soup = BeautifulSoup(response.content, "html.parser")
+    images = soup.find_all('img')
+    while(True):
+       for counter in images:
+          image_url = counter.get('src')
+          data.append([image_url])
+          if (image_url != ""):
+              print("Cool")
+              i += 1
+          if (i > 999): break
+          page += 1
+
+
+
+
+
+
+
--- scraper.py
+++ scraper.py
@@ -1,11 +1,3 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-
-url1 = 'https://yandex.ru/images/search?text=tiger'
-url2 = 'https://yandex.ru/images/search?text=leopard'
-response = requests.get(url1)
-soup = BeautifulSoup(response.text, 'lxml')
 
 
 

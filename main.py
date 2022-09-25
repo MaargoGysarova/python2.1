e7e7a936d6e8371bc07de7e8c3840f96e03a3171
margogusarova <margogusarova19@gmail.com>
2022-09-25 15:55:52 +0400

    Cleaning code

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 import os, shutil
 import requests
 from bs4 import BeautifulSoup
+HEADERS = {"User-Agent": "Mozilla/5.0"}
 
-url1 = 'https://yandex.ru/images/search?text=tiger'
-url2 = 'https://yandex.ru/images/search?text=leopard'
-# response = requests.get(url1)
-# soup = BeautifulSoup(response.text, 'lxml')
-
-# os.mkdir("dataset")
-# os.mkdir("dataset/" + "tiger")
-# os.mkdir("dataset/" + "leopard")
-
-# images = soup.find_all('img')
+# url1 = 'https://yandex.ru/images/search?text=tiger'
+# url2 = 'https://yandex.ru/images/search?text=leopard'
+def download_image(image_url, name, i):
+    with open(f"dataset/{name}/{i:04d}.jpg", "wb") as f:
+        im = requests.get(f"https:{image_url}")
+        f.write(im.content)
 
 def get_image_url(name):
     i = 1
     page = 0
-    os.mkdir("dataset/" + "{name}")
-    response = requests.get("https://yandex.ru/images/search?p={page}&text={name}&lr=51&rpt=image")
-    data = []
+    os.mkdir(f"dataset/{name}")
+    response = requests.get(f"https://yandex.ru/images/search?p={page}&text={name}&lr=51&rpt=image" , headers=HEADERS)
     soup = BeautifulSoup(response.text, "html.parser")
     images = soup.find_all('img')
     while(True):
        for image in images:
           image_url = image.get('src')
-          data.append([image_url])
+          if (i > 999):
+              break
           if (image_url != ""):
-              print("Cool")
+              download_image(image_url,name,i)
               i += 1
-          if (i > 999): break
-          page += 1
-
-def download_image(image_url,name,i):
- with open("dataset/name/'{i:04d}.jpg','wb") as f:
-     im=requests.get(image_url)
-     f.write(im.content)
+       if (i > 999):
+           break
+       page += 1
 
 def clear_folder(name):
     shutil.rmtree(name)
@@ -45,3 +38,7 @@ def cheak_folder():
     except:
         clear_folder("dataset")
         os.mkdir("dataset")
+
+cheak_folder()
+get_image_url("tiger")
+get_image_url("leopard")

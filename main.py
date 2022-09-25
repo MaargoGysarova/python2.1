167ae272d873aa705da397e2e38f15d4f1ce565a
margogusarova <margogusarova19@gmail.com>
2022-09-25 13:46:31 +0400

    add function to download image in folder dataset/name with use manager 'with/as'

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from bs4 import BeautifulSoup
 
 url1 = 'https://yandex.ru/images/search?text=tiger'
 url2 = 'https://yandex.ru/images/search?text=leopard'
-#response = requests.get(url1)
-#soup = BeautifulSoup(response.text, 'lxml')
+# response = requests.get(url1)
+# soup = BeautifulSoup(response.text, 'lxml')
 
 os.mkdir("dataset")
 os.mkdir("dataset/" + "tiger")
 os.mkdir("dataset/" + "leopard")
 
-#images = soup.find_all('img')
+# images = soup.find_all('img')
 
 def get_image_url(name):
     i = 1
     page = 0
     response = requests.get("https://yandex.ru/images/search?p={page}&text={name}&lr=51&rpt=image")
     data = []
-    soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(page.text, "html.parser")
     images = soup.find_all('img')
     while(True):
-       for counter in images:
-          image_url = counter.get('src')
+       for image in images:
+          image_url = image.get('src')
           data.append([image_url])
           if (image_url != ""):
               print("Cool")
@@ -30,9 +30,7 @@ def get_image_url(name):
           if (i > 999): break
           page += 1
 
-
-
-
-
-
-
+def download_image(url,name,i):
+ with open("dataset/name/'{i:04d}.jpg','wb") as f:
+     im=requests.get(url)
+     f.write(im.content)
